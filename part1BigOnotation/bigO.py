956b0bafc4e5d47248950a4e26294cd4344b51d4
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-07 19:38:00 +0000

    the 3 possible big O scenarios. In the real world

---
+++ part1BigOnotation/bigO.py
@@ -0,0 +1,19 @@
+"""
+It's worthwhile at this point to explain that Big O can be used to determine three cases with an algorithm:
+
+        * Best Case: In the telephone book search, the best case is that we find the name in one comparison.
+        This is O(1) or constant complexity;
+
+        * Expected Case: As discussed above this is O(log n); and
+
+        * Worst Case: This is also O(log n).
+"""
+
+
+
+
+
+
+
+
+
