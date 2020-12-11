6c437fe503d4bf0c675692249b22b9657ad81e9b
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-11 13:57:17 +0000

    adding more test to be run

--- part2Arrays/fideMissingElement.py
+++ part2Arrays/fideMissingElement.py
@@ -76,7 +76,7 @@ from nose.tools import assert_equal
 
 class TestFinder(object):
 
-    def test(self,sol):
+    def test(self, sol):
         assert_equal(sol([5, 5, 7, 7], [5, 7, 7]), 5)
         assert_equal(sol([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
         assert_equal(sol([9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 8, 7, 5, 4, 3, 2, 1]), 6)
@@ -86,6 +86,8 @@ class TestFinder(object):
 # Run test
 t = TestFinder()
 t.test(finder)
+t.test(finder2)
+t.test(finder3)
 
 
 
