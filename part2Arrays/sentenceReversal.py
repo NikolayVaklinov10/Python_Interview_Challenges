bac3db7878d217194eb6f16d4fef880ea99f1d39
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-11 19:20:52 +0000

    the third version of the code

--- part2Arrays/sentenceReversal.py
+++ part2Arrays/sentenceReversal.py
@@ -25,6 +25,42 @@ def rev_word2(s):
     return " ".join(s.split()[::-1])
 
 
+# A more efficient approach
+def rev_word3(s):
+    """
+    Manually doing the splits on the spaces.
+    """
+
+    words = []
+    length = len(s)
+    spaces = [' ']
+
+    # Index Tracker
+    i = 0
+
+    # While index is less than length of string
+    while i < length:
+
+        # If element isn't a space
+        if s[i] not in spaces:
+
+            # The word starts at this index
+            word_start = i
+
+            while i < length and s[i] not in spaces:
+
+                # Get index where word ends
+                i += 1
+            # Append that word to the list
+            words.append(s[word_start:i])
+        # Add to index
+        i += 1
+
+    # Join the reversed words
+    return " ".join(reversed(words))
+
+
+
 
 
 
