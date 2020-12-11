5f6c461f8136fa76adc4798f2a790050ba9d66af
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-11 15:46:24 +0000

    the testing part of the code

--- part2Arrays/largestContinuousSum.py
+++ part2Arrays/largestContinuousSum.py
@@ -39,6 +39,24 @@ def large_count_sum(arr):
 print(large_count_sum([1,2,-1,3,4,10,10,-10,-1]))
 
 
+"""
+Test Your Solution
+"""
+
+from nose.tools import assert_equal
+
+
+class LargeContTest(object):
+    def test(self, sol):
+        assert_equal(sol([1, 2, -1, 3, 4, -1]), 9)
+        assert_equal(sol([1, 2, -1, 3, 4, 10, 10, -10, -1]), 29)
+        assert_equal(sol([-1, 1]), 1)
+        print('ALL TEST CASES PASSED')
+
+
+# Run Test
+t = LargeContTest()
+t.test(large_count_sum)
 
 
 
