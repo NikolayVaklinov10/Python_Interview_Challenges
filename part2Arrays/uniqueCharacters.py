dfdf8bcfd4e4170e1473f2f1fd8c8403ae711f70
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-11 23:05:13 +0000

    the first version of the challenge

---
+++ part2Arrays/uniqueCharacters.py
@@ -0,0 +1,25 @@
+"""
+Unique Characters in String
+Problem
+Given a string,determine if it is comprised of all unique characters. For example, the string 'abcde' has all unique
+ characters and should return True. The string 'aabcde' contains duplicate characters and should return false.
+Solution
+We'll show two possible solutions, one using a built-in data structure and a built in function, and another using a
+built-in data structure but using a look-up method to check if the characters are unique.
+"""
+
+
+def unique_characters(s):
+    return len(set(s)) == len(set(s))
+
+
+
+
+
+
+
+
+
+
+
+
