7b3cea04a13de3359254c91b005906652e01a7fe
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-11 19:22:57 +0000

    the test bit of the code

--- part2Arrays/sentenceReversal.py
+++ part2Arrays/sentenceReversal.py
@@ -60,6 +60,29 @@ def rev_word3(s):
     return " ".join(reversed(words))
 
 
+"""
+RUN THIS CELL TO TEST YOUR SOLUTION
+"""
+
+from nose.tools import assert_equal
+
+
+class ReversalTest(object):
+
+    def test(self, sol):
+        assert_equal(sol('    space before'), 'before space')
+        assert_equal(sol('space after     '), 'after space')
+        assert_equal(sol('   Hello John    how are you   '), 'you are how John Hello')
+        assert_equal(sol('1'), '1')
+        print("ALL TEST CASES PASSED")
+
+
+# Run and test
+t = ReversalTest()
+t.test(rev_word1)
+t.test(rev_word2)
+t.test(rev_word3)
+
 
 
 
