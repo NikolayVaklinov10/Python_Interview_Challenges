cb7aa177e74ad951a564611d9c5d0323ce7c26a8
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-11 22:33:39 +0000

    the test bit of the code

--- part2Arrays/stringCompression.py
+++ part2Arrays/stringCompression.py
@@ -46,5 +46,23 @@ def compression(s):
     return r
 
 
+"""
+RUN THIS CELL TO TEST YOUR SOLUTION
+"""
+from nose.tools import assert_equal
+
+
+class TestCompress(object):
+
+    def test(self, sol):
+        assert_equal(sol(''), '')
+        assert_equal(sol('AABBCC'), 'A2B2C2')
+        assert_equal(sol('AAABCCDDDDD'), 'A3B1C2D5')
+        print('ALL TEST CASES PASSED')
+
+
+# Run Tests
+t = TestCompress()
+t.test(compression)
 
 
