5789110a16c2c5be1f98242b0af813ce0647e8f3
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-13 22:03:59 +0200

    singly linked list

---
+++ part4LinkedLists/singlyLinkedlist.py
@@ -0,0 +1,27 @@
+class Node(object):
+
+    def __init__(self, value):
+
+        self.value = value
+        self.next_node = Node
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+a.next_node = b
+b.next_node = c
+
+print(a.next_node.value)
+
+
+
+
+
+
+
+
+
+
+
