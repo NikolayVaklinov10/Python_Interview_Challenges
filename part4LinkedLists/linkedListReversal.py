ec527ad9e77f6dd805ce8389dba820d02573b4fc
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-14 21:28:27 +0200

    the reversal linked list code

--- part4LinkedLists/linkedListReversal.py
+++ part4LinkedLists/linkedListReversal.py
@@ -1,36 +1,51 @@
-
 class Node(object):
 
     def __init__(self, value):
-
         self.value = value
-        self.next_node = Node
+        self.nextnode = None
 
 
 def reverse(head):
-
-    # Set up current, previous, and next nodes
+    # Set up current,previous, and next nodes
     current = head
-    previous = Node
-    next_node = Node
+    previous = None
+    nextnode = None
 
     # until we have gone through to the end of the list
     while current:
-
         # Make sure to copy the current nodes next node to a variable next_node
         # Before overwriting as the previous node for reversal
-        next_node = current.next_node
+        nextnode = current.nextnode
 
-        # Reverse the pointer to the next_node
-        current.next_node = previous
+        # Reverse the pointer ot the next_node
+        current.nextnode = previous
 
         # Go one forward in the list
         previous = current
-        current = next_node
+        current = nextnode
 
     return previous
 
+"""
+Testing the code
+"""
 
+# Create a list of 4 nodes
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
 
+# Set up order a,b,c,d with values 1,2,3,4
+a.nextnode = b
+b.nextnode = c
+c.nextnode = d
+
+print(a.nextnode.value)
+print(b.nextnode.value)
+print(c.nextnode.value)
+print("------------------------------------")
+print(reverse(a))
+print(a.nextnode.value)
 
 
