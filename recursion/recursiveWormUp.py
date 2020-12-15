800bbc3d80c85037d64d716dc90e03042f1ef910
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-15 22:13:48 +0200

    the cumulative sum function

---
+++ recursion/recursiveWormUp.py
@@ -0,0 +1,37 @@
+# 1) Calculate the cumulative sum
+def rec_sum(n):
+
+    # Base Case
+    if n == 0:
+        return 0
+    else:
+        return n + rec_sum(n - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
