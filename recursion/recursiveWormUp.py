78c2dbbe092840cc88c333c1364e9f8dfa3824ab
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-15 22:29:26 +0200

    the sum of all integers out of a number

--- recursion/recursiveWormUp.py
+++ recursion/recursiveWormUp.py
@@ -8,6 +8,16 @@ def rec_sum(n):
         return n + rec_sum(n - 1)
 
 
+# 2) Return the sum of all individual digits in that integer
+def sum_func(n):
+
+    # Base Case
+    if len(str(n)) == 1:
+        return n
+    else:
+        return n % 10 + sum_func(n/10)
+
+
 
 
 
