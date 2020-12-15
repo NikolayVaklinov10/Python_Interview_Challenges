407bf5c7cbc09667b21d69c3cb81d3fd990cf5e6
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-15 23:21:03 +0200

    the word split function

--- recursion/recursiveWormUp.py
+++ recursion/recursiveWormUp.py
@@ -18,26 +18,19 @@ def sum_func(n):
         return n % 10 + sum_func(n/10)
 
 
+# 3) A function to create possible
+def word_split(phrase, list_of_words, output=None):
 
+    if output is None:
+        output = []
 
+    for word in list_of_words:
+        if phrase.startsWith(word):
+            output.append(word)
 
+            return word_split( phrase[len(word):], list_of_words, output)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return output
 
 
 
