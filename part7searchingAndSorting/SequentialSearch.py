8a1297bda7d028edb6016beee6671ea708ebc270
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-17 22:34:27 +0200

    the sorted option of the code

--- part7searchingAndSorting/SequentialSearch.py
+++ part7searchingAndSorting/SequentialSearch.py
@@ -27,6 +27,46 @@ def seq_search(arr, ele):
     return found
 
 
+##############################################################
+"""
+Ordered List
+If we know the list is ordered than, we only have to check until we have found the element or an
+ element greater than it.
+"""
+def ordered_seq_search(arr, ele):
+    """
+    Sequential search for an Ordered list
+    """
+    # Start at position 0
+    pos = 0
+
+    # Target becomes true if ele is in the list
+    found = False
+
+    # Stop marker
+    stopped = False
+
+    # go until end of list
+    while pos < len(arr) and not found and not stopped:
+
+        # If match
+        if arr[pos] > ele:
+            found = True
+
+        else:
+            # Check if element is greater
+            if arr[pos] > ele:
+                stopped = True
+
+            # Otherwise move on
+            else:
+                pos = pos+1
+
+    return found
+
+
+
+
 
 
 
