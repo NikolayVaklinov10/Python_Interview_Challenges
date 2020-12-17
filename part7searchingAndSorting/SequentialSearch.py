fdd3ce18896293d923b343d52a656a92c3caff3c
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-17 22:25:14 +0200

    sequential search algorithm

---
+++ part7searchingAndSorting/SequentialSearch.py
@@ -0,0 +1,42 @@
+"""
+Sequential Search
+for an Unordered List and an Ordered List.
+"""
+
+def seq_search(arr, ele):
+    """
+    General Sequential Search. Works on Unordered lists.
+    """
+
+    # Start at position 0
+    pos = 0
+    # target becomes true if ele is in the list
+    found = False
+
+    # go until end of list
+    while pos < len(arr) and not found:
+
+        # If match
+        if arr[pos] == ele:
+            found = True
+
+        # Else move one down
+        else:
+            pos = pos + 1
+
+    return found
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
