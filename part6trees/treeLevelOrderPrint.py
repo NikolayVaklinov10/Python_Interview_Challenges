2646bbc0006a30608b85c1f2e42446a74ac2200e
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-17 18:41:56 +0200

    the level order print function

--- part6trees/treeLevelOrderPrint.py
+++ part6trees/treeLevelOrderPrint.py
@@ -7,6 +7,37 @@ class Node:
         self.val = val
 
 
+import collections
+
+
+def level_order_print(tree):
+
+    if not tree:
+        return
+
+    nodes = collections.deque([tree])
+
+    current_count = 1
+    next_count = 0
+
+    while len(nodes) != 0:
+
+        current_node = nodes.popleft()
+        current_count -= 1
+
+        print(current_node.val)
+
+        if current_node.left:
+            nodes.append(current_node.left)
+            next_count += 1
+
+        if current_node.right:
+            nodes.append(current_node.right)
+            next_count += 1
+        if current_count == 0:
+            print('\n')
+            current_count, next_count = next_count, current_count
+
 
 
 
