b9373b83519207591d2450de0177589657eb0516
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-20 17:08:30 +0200

    the path bit

--- part8Graphs/breadthFirstSearch.py
+++ part8Graphs/breadthFirstSearch.py
@@ -20,6 +20,19 @@ def bfs(graph, start):
 bfs(graph, 'A')
 
 
+# Paths
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path +[next]))
+
+
+list(bfs_paths(graph, 'A', 'F'))
 
 
 
