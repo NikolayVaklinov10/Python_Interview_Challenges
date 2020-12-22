70b50c1664fd624e3317434601c03ccf9c31dd56
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-22 16:45:08 +0200

    the enhanced version

--- part9mock1E-commerce/onSiteQuestion1.py
+++ part9mock1E-commerce/onSiteQuestion1.py
@@ -35,3 +35,35 @@ def profit(stock_prices):
 
 
 print(profit([10, 12, 14, 12, 13, 11, 8, 7, 6, 13, 23, 45, 11, 10]))
+
+
+def profit2(stock_prices):
+
+    # Check length
+    if len(stock_prices) < 2:
+        raise Exception("Need at least two stock prices!")
+
+    # Start minimum prices marker at first prices
+    min_stock_price = stock_prices[0]
+
+    # Start off with an initial max profit
+    max_profit = stock_prices[1] - stock_prices[0]
+
+    # Skip first index of 0
+    for price in stock_prices[1:]:
+        # NOTE THE REORDERING HERE DUE TO THE NEGATIVE PROFIT TRACKING
+
+        # Check the current price against our minimum for a profit
+        # comparison against the max_profit
+        comparison_profit = price - min_stock_price
+
+        # Compare against our max_profit so far
+        max_profit = max(max_profit, comparison_profit)
+
+        # Check to set the lowest stock price so far
+        min_stock_price = min(min_stock_price, price)
+
+    return max_profit
+
+
+
