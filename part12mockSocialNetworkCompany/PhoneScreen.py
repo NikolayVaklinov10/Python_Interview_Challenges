f494e8bc3a089d9f253c7c70f00df493c8cc1d78
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-24 15:04:45 +0200

    remove duplicate

---
+++ part12mockSocialNetworkCompany/PhoneScreen.py
@@ -0,0 +1,14 @@
+def remove_duplicates(string):
+
+    result = []
+    seen = set()
+
+    for char in string:
+        if char not in seen:
+            seen.add(char)
+            result.append(char)
+
+    return ''.join(result)
+
+
+
